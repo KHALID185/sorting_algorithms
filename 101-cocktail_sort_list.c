7605b22938c3f37c5d705b7ccb27957d0fef0711
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-18 09:24:32 -0800

    sorting algrthm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,60 +1,59 @@
 #include "sort.h"
 
 /**
- * swapme - swap the nodes themselves.
- * @current: pointer.
- * @current_old: pointer.
- * @list: doubly linked list
- */
-void swapme(listint_t *current, listint_t *current_old, listint_t **list)
-{
-	listint_t *temp1 = current->next;
-	listint_t *temp2 = current_old->prev;
+ * ech_place - echange value in the array
+ * @index_new: pointer in a new value of array
+ * @index_prev: pointer in the old value of array
+ * @d_lst: a doubly linked list
+ * Return: empty
+*/
 
-	if (temp1 != NULL)
-		temp1->prev = current_old;
-	if (temp2 != NULL)
-		temp2->next = current;
-	current->prev = temp2;
-	current_old->next = temp1;
-	current->next = current_old;
-	current_old->prev = current;
-	if (*list == current_old)
-		*list = current;
-	print_list(*list);
+void ech_place(listint_t *index_new, listint_t *index_prev, listint_t **d_lst)
+{
+	listint_t *garage_1 = index_new->next;
+	listint_t *garage_2 = index_prev->prev;
+
+	if (garage_1)
+		garage_1->prev = index_prev;
+	if (garage_2)
+		garage_2->next = index_new;
+	index_new->prev = garage_2;
+	index_prev->next = garage_1;
+	index_new->next = index_prev;
+	index_prev->prev = index_new;
+	if (*d_lst == index_prev)
+		*d_lst = index_new;
+	print_list(*d_lst);
 }
 
 /**
- * cocktail_sort_list - cocktail_sort_list
- *
+ * cocktail_sort_list - sort array of int using cocktail sort algorithm
  * @list: doubly linked list
- */
+ * Return: empty
+*/
+
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *check = *list, *first = NULL, *last = NULL;
+	listint_t *srt_tail = *list, *el_f = NULL, *el_l = NULL;
 
-	if (!list)
-		return;
-	if (!(*list))
-		return;
-	if (!(*list)->next)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 	do {
-		while (check->next)
+		while (srt_tail->next)
 		{
-			if (check->n > check->next->n)
-				swapme(check->next, check, list);
+			if (srt_tail->n > srt_tail->next->n)
+				ech_place(srt_tail->next, srt_tail, list);
 			else
-				check = check->next;
+				srt_tail = srt_tail->next;
 		}
-		last = check;
-		while (check->prev != first)
+		el_l = srt_tail;
+		while (srt_tail->prev != el_f)
 		{
-			if (check->n < check->prev->n)
-				swapme(check, check->prev, list);
+			if (srt_tail->n < srt_tail->prev->n)
+				ech_place(srt_tail, srt_tail->prev, list);
 			else
-				check = check->prev;
+				srt_tail = srt_tail->prev;
 		}
-		first = check;
-	} while (first != last);
+		el_f = srt_tail;
+	} while (el_f != el_l);
 }
