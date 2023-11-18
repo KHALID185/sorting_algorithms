d342395a2e3283d1c3f9408554e174f3d5bae30b
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-18 09:38:37 -0800

    sorting algrthm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,62 +1,61 @@
 #include "sort.h"
 
-void move_left(listint_t *curr, listint_t *prev, listint_t **head);
+void ech_place(listint_t *index_new, listint_t *index_prev, listint_t **d_lst);
 
 /**
- * cocktail_sort_list - coctail sort in doubly linked list
- * @list: A Doubly linked list
- */
+ * cocktail_sort_list - sort array of int using cocktail sort algorithm
+ * @list: doubly linked list
+ * Return: empty
+*/
 
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *cur;
-	listint_t *max = NULL;
-	listint_t *min = NULL;
+	listint_t *srt_tail = *list, *el_f = NULL, *el_l = NULL;
 
 	if (!list || !(*list) || (*list)->next == NULL)
 		return;
-	cur = *list;
 	do {
-		while (cur->next)
+		while (srt_tail->next)
 		{
-			if (cur->n > cur->next->n)
-				move_left(cur->next, cur, list);
+			if (srt_tail->n > srt_tail->next->n)
+				ech_place(srt_tail->next, srt_tail, list);
 			else
-				cur = cur->next;
+				srt_tail = srt_tail->next;
 		}
-		max = cur;
-		while (cur->prev != min)
+		el_l = srt_tail;
+		while (srt_tail->prev != el_f)
 		{
-			if (cur->n < cur->prev->n)
-				move_left(cur, cur->prev, list);
+			if (srt_tail->n < srt_tail->prev->n)
+				ech_place(srt_tail, srt_tail->prev, list);
 			else
-				cur = cur->prev;
+				srt_tail = srt_tail->prev;
 		}
-		min = cur;
-	} while (min != max);
+		el_f = srt_tail;
+	} while (el_f != el_l);
 }
 
 /**
- * move_left - swaps two members of a list
- *
- * @curr: current node
- * @prev: previous node
- * @head: head of list
- */
-void move_left(listint_t *curr, listint_t *prev, listint_t **head)
-{
-	listint_t *swap1 = curr->next;
-	listint_t *swap2 = prev->prev;
+ * * ech_place - echange value in the array
+ * * @index_new: pointer in a new value of array
+ * * @index_prev: pointer in the old value of array
+ * * @d_lst: a doubly linked list
+ * * Return: empty
+ * */
 
-	if (swap1 != NULL)
-		swap1->prev = prev;
-	if (swap2 != NULL)
-		swap2->next = curr;
-	curr->prev = swap2;
-	prev->next = swap1;
-	curr->next = prev;
-	prev->prev = curr;
-	if (*head == prev)
-		*head = curr;
-	print_list(*head);
+void ech_place(listint_t *index_new, listint_t *index_prev, listint_t **d_lst)
+{
+	listint_t *garage_1 = index_new->next;
+	listint_t *garage_2 = index_prev->prev;
+
+	if (garage_1 != NULL)
+		garage_1->prev = index_prev;
+	if (garage_2 != NULL)
+		garage_2->next = index_new;
+	index_new->prev = garage_2;
+	index_prev->next = garage_1;
+	index_new->next = index_prev;
+	index_prev->prev = index_new;
+	if (*d_lst == index_prev)
+		*d_lst = index_new;
+	print_list(*d_lst);
 }
--- garage.c
+++ garage.c
@@ -1,30 +1,6 @@
 #include "sort.h"
 
-/**
- * ech_place - echange value in the array
- * @index_new: pointer in a new value of array
- * @index_prev: pointer in the old value of array
- * @d_lst: a doubly linked list
- * Return: empty
-*/
-
-void ech_place(listint_t *index_new, listint_t *index_prev, listint_t **d_lst)
-{
-	listint_t *garage_1 = index_new->next;
-	listint_t *garage_2 = index_prev->prev;
-
-	if (garage_1)
-		garage_1->prev = index_prev;
-	if (garage_2)
-		garage_2->next = index_new;
-	index_new->prev = garage_2;
-	index_prev->next = garage_1;
-	index_new->next = index_prev;
-	index_prev->prev = index_new;
-	if (*d_lst == index_prev)
-		*d_lst = index_new;
-	print_list(*d_lst);
-}
+void ech_place(listint_t *index_new, listint_t *index_prev, listint_t **d_lst);
 
 /**
  * cocktail_sort_list - sort array of int using cocktail sort algorithm
@@ -56,4 +32,30 @@ void cocktail_sort_list(listint_t **list)
 		}
 		el_f = srt_tail;
 	} while (el_f != el_l);
+}
+
+/**
+ * * ech_place - echange value in the array
+ * * @index_new: pointer in a new value of array
+ * * @index_prev: pointer in the old value of array
+ * * @d_lst: a doubly linked list
+ * * Return: empty
+ * */
+
+void ech_place(listint_t *index_new, listint_t *index_prev, listint_t **d_lst)
+{
+	listint_t *garage_1 = index_new->next;
+	listint_t *garage_2 = index_prev->prev;
+
+	if (garage_1 != NULL)
+		garage_1->prev = index_prev;
+	if (garage_2 != NULL)
+		garage_2->next = index_new;
+	index_new->prev = garage_2;
+	index_prev->next = garage_1;
+	index_new->next = index_prev;
+	index_prev->prev = index_new;
+	if (*d_lst == index_prev)
+		*d_lst = index_new;
+	print_list(*d_lst);
 }
